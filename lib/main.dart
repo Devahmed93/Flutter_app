572c38d1a1006368e6fb6d84962df3ee64004c44
ahmed <ahmedseyam1993@gmail.com>
2019-07-22 22:28:19 +0200

    release 1

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() =>  runApp(MyApp()); //only in onestatement
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('List'),
+      ),
+      body:Card(child:Column(children: <Widget>[
+        Image.asset('assets/food.jpg'),
+        Text('food')
+      ],),),
+    ));
+  }
+}
