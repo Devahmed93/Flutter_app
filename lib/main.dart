8f786ca0dc9d1f7a9e40a38a875759c9fdeae571
ahmed <ahmedseyam1993@gmail.com>
2019-07-27 16:01:03 +0200

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() =>  runApp(MyApp()); //only in onestatement
+void main() => runApp(MyApp()); //only in onestatement
 
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['food tester'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('List'),
-        title: Text('List'),
-      ),
-      body:Card(child:Column(children: <Widget>[
-        
-      ],),),
-    ));
+            appBar: AppBar(
+              title: Text('List'),
+            ),
+            body: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                         _products.add('advanced food');
+                      
+                      });
+                     
+                    },
+                    child: Text('Add product'),
+                  ),
+                ),
+                Column(
+                  children: _products
+                      .map((element) => Card(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('assets/food.jpg'),
+                                Text(element)
+                              ],
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ],
+            )));
   }
 }
